3d052fea88f6bbdecc1940e5f4bc66ff796e8cb2
Ken Yeh <yehken@gmail.com>
2024-11-08 00:33:50 -0800

    start lab

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 
+function Header() {
+  return (<h1>Ken's Simple React Application</h1>);
+}
+
+function Content() {
+  return (<p>This is my Per Scholas lab</p>);
+}
+
+function Footer() {
+  return (<h1>Created by Me, of course.</h1>);
+}
 function App() {
   return (
     <>
+    <Header />
+    <Content />
+    <Footer />
     </>
   );
 }
